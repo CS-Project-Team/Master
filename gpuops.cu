03e6e11f607874a574a20cfd8e47de32384ca740
CS-Project-Team <lrenades@jarvis.cs.iit.edu>
2014-09-22 18:14:21 -0500

    added function to get number of cores

--- gpuops.cu
+++ gpuops.cu
@@ -100,10 +100,50 @@ void speed_test_float(int grids, int blocks){
 }
 
 
+int ConvertSMVer2Cores(int major, int minor)
+{
+        // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+        typedef struct {
+                int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+                int Cores;
+        } sSMtoCores;
+
+        sSMtoCores nGpuArchCoresPerSM[] =
+        { { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
+          { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
+          { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
+          { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
+          { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+          { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+          { 0x30, 192}, // Fermi Generation (SM 3.0) GK10x class
+          {   -1, -1 }
+        };
+
+        int index = 0;
+        while (nGpuArchCoresPerSM[index].SM != -1) {
+                if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
+                        return nGpuArchCoresPerSM[index].Cores;
+                }
+                index++;
+        }
+        printf("MapSMtoCores SM %d.%d is undefined (please update to the latest SDK)!\n", major, minor);
+        return -1;
+}
+
+
 TestResult gpu_test() {
 	TestResult result;
 	int n_blocks = 1, n_grids = 1; //get actual number of cores here
 	
+	int dev = 0;	
+	cudaSetDevice(0);
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+	printf("\n  (%2d) Multiprocessors, (%3d) CUDA Cores/MP:     %d CUDA Cores\n",
+               deviceProp.multiProcessorCount,
+               ConvertSMVer2Cores(deviceProp.major, deviceProp.minor),
+               ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) * deviceProp.multiProcessorCount);	
 	for(int i = 0; i < N_TESTS; i++) {
 		speed_test_int(n_grids, n_blocks);
 		result.int_times[i] = 0;
